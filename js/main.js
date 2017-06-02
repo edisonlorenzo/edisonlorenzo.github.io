9e2b4c6c80b40c68e885bff3642bbf831358bc1e
Edison Lorenzo <edison.lorenzo@flightdigitalmedia.com>
2017-06-02 17:07:53 +0800

    added fix for parsing spine json file

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 
+if(window.jsonic) {
+	// kludge for spines being in JS rather than json
+	JSON.parse = jsonic;
+}
+
 var assetLoaderManager = AssetLoaderManager.getInstance();
 var stageManager = StageManager.getInstance();
 var spineManager = SpineManager.getInstance();
@@ -12,8 +17,10 @@ assetLoaderManager.load();
 function assetReady()
 {
     spineManager.createSpine("powercore_male","Edison",100,250,0.5);  
-    spineManager.createSpine("powercore_male","Jia",250,250,0.5);  
-    spineManager.createSpine("powercore_male","Jian",400,250,0.5);  
+    spineManager.createSpine("powercore_male","Jia",300,250,0.5);  
+    spineManager.createSpine("powercore_male","Jian",500,250,0.5);  
+    spineManager.createSpine("popple","Popple",100,500,0.5); 
+    spineManager.createSpine("popple","Pico",300,500,0.5);  
 }
 
 
