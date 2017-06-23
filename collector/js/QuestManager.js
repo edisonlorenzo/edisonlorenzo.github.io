1d1a9ec03e4aa4279a4d1f1a8482fde56ce38049
Edison Lorenzo <edison.lorenzo@flightdigitalmedia.com>
2017-06-23 18:35:46 +0800

    - added help dialog box - reworked the card view layout

--- collector/js/QuestManager.js
+++ collector/js/QuestManager.js
@@ -14,7 +14,7 @@ var QuestManager = (function () {
         var backgroundContainer;
         var footerContainer;
         var questContainer;
-        var cardContainer;
+        var dialogContainer;
         
         var assets = new Array();
         
@@ -29,7 +29,9 @@ var QuestManager = (function () {
         assets.push(new Asset('tc_quest_header_small', 'images/tc_quest_header_small.png'));
         assets.push(new Asset('q_medallion', 'images/q_medallion.png'));
         assets.push(new Asset('q_card', 'images/q_card.png'));
+        assets.push(new Asset('q_help_info', 'images/q_help_info.png'));
         assets.push(new Asset('tc_close', 'images/tc_close.png'));
+        assets.push(new Asset('rounded_rectangle', 'images/rounded_rectangle.png'));
         
         function Asset(resName, resPath)
         {
@@ -67,12 +69,12 @@ var QuestManager = (function () {
             backgroundContainer = new PIXI.Container();
             footerContainer = new PIXI.Container();
             questContainer = new PIXI.Container();
-            cardContainer = new PIXI.Container();
+            dialogContainer = new PIXI.Container();
 
             stageManager.getContainer().addChild(backgroundContainer);
             stageManager.getContainer().addChild(questContainer);
             stageManager.getContainer().addChild(footerContainer);
-            stageManager.getContainer().addChild(cardContainer);
+            stageManager.getContainer().addChild(dialogContainer);
             
             res =  AssetLoaderManager.getInstance().getRes();
 
@@ -135,15 +137,27 @@ var QuestManager = (function () {
             
             var collectBtn = createImage('footer_collectBtn', footerContainer, res['q_collect'].texture);
             collectBtn.image.anchor.y = 1;
-            collectBtn.image.scale.x = collectBtn.image.scale.y = stageManager.getDimension().calculateRatioByWidth(collectBtn.image.width, 0.18);
+            collectBtn.image.scale.x = collectBtn.image.scale.y = stageManager.getDimension().calculateRatioByWidth(collectBtn.image.width, 0.2);
             collectBtn.image.position.y = stageManager.getDimension().height;
             collectBtn.image.position.x = 8;
             
             var helpBtn = createImage('footer_helpBtn', footerContainer, res['q_help'].texture);
             helpBtn.image.anchor.y = 1;
-            helpBtn.image.scale.x = helpBtn.image.scale.y = stageManager.getDimension().calculateRatioByWidth(helpBtn.image.width, 0.12);
+            helpBtn.image.scale.x = helpBtn.image.scale.y = stageManager.getDimension().calculateRatioByWidth(helpBtn.image.width, 0.15);
             helpBtn.image.position.y = stageManager.getDimension().height;
             helpBtn.image.position.x = footerObj.image.width - helpBtn.image.width - 5;
+            helpBtn.image.interactive = true;
+            helpBtn.image.on('pointertap', function () {
+                if(!helpBtn.hasClicked)
+                {
+                    helpBtn.hasClicked = true;
+                    showHelp(function()
+                    {
+                        helpBtn.hasClicked = false;
+                    });
+                }
+
+            });
             
             var statusObj = createImage('footer_status', footerContainer, res['q_status_bg'].texture);
             statusObj.image.scale.x = stageManager.getDimension().calculateRatioByWidth(statusObj.image.width, 0.6);
@@ -151,29 +165,37 @@ var QuestManager = (function () {
             statusObj.image.position.y = footerObj.image.y + 12;
             statusObj.image.position.x = (stageManager.getDimension().width * 0.5) - (statusObj.image.width * 0.5) + 15;
             
-            var cardBgObj = createImage('cardQuest_background', cardContainer, res['white'].texture);
-            cardBgObj.image.visible = false;
-            cardBgObj.image.tint = 0x000000;
-            cardBgObj.image.alpha = 0.5;
-            cardBgObj.image.interactive = true;
-            cardBgObj.image.anchor.set(0.5);
-            cardBgObj.image.scale.x = stageManager.getDimension().calculateRatioByWidth(cardBgObj.image.width, 1);
-            cardBgObj.image.scale.y = stageManager.getDimension().calculateRatioByHeight(cardBgObj.image.height, 1);
-            cardBgObj.image.position.y = stageManager.getDimension().height * 0.5;
-            cardBgObj.image.position.x = stageManager.getDimension().width * 0.5;
+            var dialogBgObj = createImage('cardQuest_background', dialogContainer, res['white'].texture);
+            dialogBgObj.image.visible = false;
+            dialogBgObj.image.tint = 0x000000;
+            dialogBgObj.image.alpha = 0.5;
+            dialogBgObj.image.interactive = true;
+            dialogBgObj.image.anchor.set(0.5);
+            dialogBgObj.image.scale.x = stageManager.getDimension().calculateRatioByWidth(dialogBgObj.image.width, 1);
+            dialogBgObj.image.scale.y = stageManager.getDimension().calculateRatioByHeight(dialogBgObj.image.height, 1);
+            dialogBgObj.image.position.y = stageManager.getDimension().height * 0.5;
+            dialogBgObj.image.position.x = stageManager.getDimension().width * 0.5;
             
-            var cardObj = createImage('cardQuest', cardContainer, res['q_card'].texture);
-            cardObj.image.visible = false;
+            
+            var cardObjContainer = new PIXI.Container();
+            cardObjContainer.visible = false;
+            dialogContainer.addChild(cardObjContainer);
+            
+            var helpObjContainer = new PIXI.Container();
+            helpObjContainer.visible = false;
+            dialogContainer.addChild(helpObjContainer);
+            
+            var cardObj = createImage('cardQuest', cardObjContainer, res['q_card'].texture);
             cardObj.image.anchor.set(0.5);
-            cardObj.image.scale.x = cardObj.image.scale.y = stageManager.getDimension().calculateRatioByHeight(cardObj.image.height, 0.6);
+            cardObj.image.scale.x = cardObj.image.scale.y = stageManager.getDimension().calculateRatioByWidth(cardObj.image.width, 0.9);
             cardObj.image.position.x =  stageManager.getDimension().width * 0.5;
             cardObj.image.position.y =  stageManager.getDimension().height * 0.5;
             
-            var cardCloseBtn = createImage('cardQuest_closeBtn', cardObj.image, res['tc_close'].texture);
+            var cardCloseBtn = createImage('cardQuest_closeBtn', cardObjContainer, res['tc_close'].texture);
             cardCloseBtn.image.anchor.set(0.5);
-            cardCloseBtn.image.scale.x = cardCloseBtn.image.scale.y = 1.5;
-            cardCloseBtn.image.position.x = cardObj.image.width - 35;
-            cardCloseBtn.image.position.y = -(cardObj.image.height) + 50;
+            cardCloseBtn.image.scale.x = cardCloseBtn.image.scale.y = 1;
+            cardCloseBtn.image.position.x = cardObj.image.position.x + (cardObj.image.width * 0.46);
+            cardCloseBtn.image.position.y = cardObj.image.position.y - (cardObj.image.height * 0.46);
             cardCloseBtn.image.interactive = true;
             cardCloseBtn.image.on('pointertap', function () {
                 if(!cardCloseBtn.hasClicked)
@@ -187,12 +209,48 @@ var QuestManager = (function () {
                 
             });
             
+            var cardImageObj = createImage('cardQuest_image', cardObjContainer, res['q_medallion'].texture);
+            cardImageObj.image.anchor.x = 0.5;
+            cardImageObj.image.scale.x = cardImageObj.image.scale.y = 1;
+            cardImageObj.image.position.x = cardObj.image.position.x;
+            cardImageObj.image.position.y = cardObj.image.position.y - (cardObj.image.height * 0.4);
+            
+            var cardContentBgObj = createImage('cardQuest_contentBg', cardObjContainer, res['rounded_rectangle'].texture);
+            cardContentBgObj.image.anchor.x = 0.5;
+            cardContentBgObj.image.scale.x = cardContentBgObj.image.scale.y = (cardObj.image.width * 0.85) / cardContentBgObj.image.width;
+            cardContentBgObj.image.position.x = cardObj.image.position.x;
+            cardContentBgObj.image.position.y = cardObj.image.position.y - 25;
+            
+            var helpObj = createImage('helpQuest', helpObjContainer, res['q_help_info'].texture);
+            helpObj.image.anchor.set(0.5);
+            helpObj.image.scale.x = helpObj.image.scale.y = stageManager.getDimension().calculateRatioByWidth(helpObj.image.width, 0.85);
+            helpObj.image.position.x =  stageManager.getDimension().width * 0.5;
+            helpObj.image.position.y =  stageManager.getDimension().height * 0.5;
+            
+            var helpCloseBtn = createImage('helpQuest_closeBtn', helpObjContainer, res['tc_close'].texture);
+            helpCloseBtn.image.anchor.set(0.5);
+            helpCloseBtn.image.scale.x = helpCloseBtn.image.scale.y = 1;
+            helpCloseBtn.image.position.x = helpObj.image.position.x + (helpObj.image.width * 0.5);
+            helpCloseBtn.image.position.y = helpObj.image.position.y - (helpObj.image.height * 0.5);
+            helpCloseBtn.image.interactive = true;
+            helpCloseBtn.image.on('pointertap', function () {
+                if(!helpCloseBtn.hasClicked)
+                {
+                    helpCloseBtn.hasClicked = true;
+                    hideHelp(function()
+                    {
+                        helpCloseBtn.hasClicked = false;
+                    });
+                }
+                
+            });
+            
             function showCard(callBack)
             {
-                TweenMax.fromTo(cardObj.image.position, 0.5, {y: stageManager.getDimension().height}, {y: stageManager.getDimension().height * 0.5, ease: Power2.easeOut, onComplete: onComplete});
-                TweenMax.fromTo(cardBgObj.image, 0.5, {alpha: 0}, {alpha: 0.5, ease: Power2.easeOut});
-                cardBgObj.image.visible = true;
-                cardObj.image.visible = true;  
+                TweenMax.fromTo(cardObjContainer.position, 0.5, {y: stageManager.getDimension().height}, {y: 0, ease: Power2.easeOut, onComplete: onComplete});
+                TweenMax.fromTo(dialogBgObj.image, 0.5, {alpha: 0}, {alpha: 0.5, ease: Power2.easeOut});
+                dialogBgObj.image.visible = true;
+                cardObjContainer.visible = true;  
                 function onComplete()
                 {
                     callBack();
@@ -201,12 +259,36 @@ var QuestManager = (function () {
 
             function hideCard(callBack)
             {
-                TweenMax.to(cardObj.image.position, 0.5, {y: stageManager.getDimension().height, ease: Power2.easeIn, onComplete: onComplete});
-                TweenMax.to(cardBgObj.image, 0.5, {alpha: 0, ease: Power2.easeIn});
+                TweenMax.to(cardObjContainer.position, 0.5, {y: stageManager.getDimension().height, ease: Power2.easeIn, onComplete: onComplete});
+                TweenMax.to(dialogBgObj.image, 0.5, {alpha: 0, ease: Power2.easeIn});
                 function onComplete()
                 {
-                    cardBgObj.image.visible = false;
-                    cardObj.image.visible = false;
+                    dialogBgObj.image.visible = false;
+                    cardObjContainer.visible = false;
+                    callBack();
+                }
+            }
+            
+            function showHelp(callBack)
+            {
+                TweenMax.fromTo(helpObjContainer.position, 0.5, {y: stageManager.getDimension().height}, {y: 0, ease: Power2.easeOut, onComplete: onComplete});
+                TweenMax.fromTo(dialogBgObj.image, 0.5, {alpha: 0}, {alpha: 0.5, ease: Power2.easeOut});
+                dialogBgObj.image.visible = true;
+                helpObjContainer.visible = true;  
+                function onComplete()
+                {
+                    callBack();
+                }
+            }
+            
+            function hideHelp(callBack)
+            {
+                TweenMax.to(helpObjContainer.position, 0.5, {y: stageManager.getDimension().height, ease: Power2.easeIn, onComplete: onComplete});
+                TweenMax.to(dialogBgObj.image, 0.5, {alpha: 0, ease: Power2.easeIn});
+                function onComplete()
+                {
+                    dialogBgObj.image.visible = false;
+                    helpObjContainer.visible = false;
                     callBack();
                 }
             }
