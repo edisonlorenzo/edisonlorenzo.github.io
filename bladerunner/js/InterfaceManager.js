34406ac94ec474f77af981e6acc458c3c99abdbc
Edison Lorenzo <edison.lorenzo@flightdigitalmedia.com>
2017-08-15 12:19:42 +0800

    test fix orientation 2

--- bladerunner/js/InterfaceManager.js
+++ bladerunner/js/InterfaceManager.js
@@ -524,8 +524,8 @@ var InterfaceManager = (function () {
                     };
 
                     /* init - you can init any event */
-                    throttle("resize", "optimizedResize");
-                    //throttle("resize", "resizeContainer")
+                    //throttle("resize", "optimizedResize");
+                    throttle("resize", "resizeContainer")
                 })();
 
                 return {
